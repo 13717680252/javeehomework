7eb874e77c502f3b55b4c0fb613afd4e78777b96
13717680252 <1271369334@qq.com>
2016-10-15 11:14:44 +0800

    Create MyResponse.java

---
+++ 14301024/src/servletcontainer/MyResponse.java
@@ -0,0 +1,96 @@
+package servletcontainer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+
+public class MyResponse implements ServletResponse {
+   public PrintWriter writer;
+    public  OutputStream opt;
+    public MyResponse(OutputStream output) {
+    	this.opt=output;
+     this.writer = new PrintWriter(opt);
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+    	  
+        return writer;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return "UTF-8";
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) {
+
+    }
+
+    @Override
+    public void setContentLength(int i) {
+
+    }
+
+    @Override
+    public void setContentType(String s) {
+
+    }
+
+    @Override
+    public void setBufferSize(int i) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+
+}
