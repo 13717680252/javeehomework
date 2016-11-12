c7d8e1ff1afaa8f1362d127a07fffb8e7b4f12c5
13717680252 <1271369334@qq.com>
2016-11-13 01:56:06 +0800

    Add files via upload

---
+++ test/src/bean/PropertyValue.java
@@ -0,0 +1,39 @@
+package bean;
+
+
+
+public class PropertyValue {
+	private boolean isConstructorParam;
+	public PropertyValue(String name, Object value,boolean isConstructorParam) {
+		this.name = name;
+		this.value = value;
+		this.isConstructorParam=isConstructorParam;
+	}
+	private String name;	
+	private Object value;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	public boolean isConstructorParam() {
+		return isConstructorParam;
+	}
+
+	public void setConstructorParam(boolean isConstructorParam) {
+		this.isConstructorParam = isConstructorParam;
+	}
+
+}
